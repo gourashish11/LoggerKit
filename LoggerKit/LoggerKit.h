ef1156034f3ccab379693df90e2f7893910b49aa
Ashish Gour <ashish_gour@external.mckinsey.com>
2024-01-28 20:34:45 +0530

    Initial Commit

---
+++ LoggerKit/LoggerKit.h
@@ -0,0 +1,18 @@
+//
+//  LoggerKit.h
+//  LoggerKit
+//
+//  Created by Ashish Gour on 28/01/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LoggerKit.
+FOUNDATION_EXPORT double LoggerKitVersionNumber;
+
+//! Project version string for LoggerKit.
+FOUNDATION_EXPORT const unsigned char LoggerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LoggerKit/PublicHeader.h>
+
+
