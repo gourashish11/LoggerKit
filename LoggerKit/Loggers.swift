30b621a62be5dc22f828843f00251175d7b42155
Ashish Gour <ashish_gour@external.mckinsey.com>
2024-01-28 20:58:04 +0530

    first commit

---
+++ LoggerKit/Loggers.swift
@@ -0,0 +1,18 @@
+//
+//  Logger.swift
+//  LoggerKit
+//
+//  Created by Ashish Gour on 28/01/24.
+//
+
+import Foundation
+
+public class Loggers {
+    
+    public init() {}
+    
+    public func log(_ message: String) {
+        print(message)
+    }
+}
+
